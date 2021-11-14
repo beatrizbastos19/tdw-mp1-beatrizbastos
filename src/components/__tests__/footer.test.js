c0c05dad21827d383e5cfb8c55ee52ac7f11306b
Beatriz Bastos <beatrizbastos@MBP-de-Beatriz.lan>
2021-11-14 23:01:49 +0000

    update

--- src/components/__tests__/footer.test.js
+++ src/components/__tests__/footer.test.js
@@ -3,7 +3,7 @@
  */
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import 'jest-dom/extend-expect'
 import Footer from '../footer'
 
 describe('footer', () => {
